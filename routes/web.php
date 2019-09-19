88874cdffc0384c973c9454387f9411ef669a0ff
wlyywf <wlyywf@gmail.com>
2019-09-19 16:06:57 +0800

    favor

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 // Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -29,4 +28,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
