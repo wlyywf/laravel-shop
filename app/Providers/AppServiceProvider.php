a3899e6452ca20e51a98d7f925996310ce4d844c
wlyywf <wlyywf@gmail.com>
2019-09-22 20:35:49 +0800

    后台管理 - 同意退款 - 支付宝退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = "http://requestbin.net/r/1g34t7c1";
+            $config['notify_url'] = "http://requestbin.net/r/13dhp011";
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
